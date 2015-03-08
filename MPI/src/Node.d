727dcf5dec899b239e19e5c5c96641dbd27b2a2d
Anthony Wertz <anthony.wertz@gmail.com>
2015-03-08 16:52:58 -0400

    Updated to build properly.

--- MPI/src/Node.d
+++
@@ -1,4 +0,0 @@
-src/Node.d: ../src/Node.cpp \
- /media/sf_Protected/Documents/Workspace/RF_MPI/inc/Node.h
-
-/media/sf_Protected/Documents/Workspace/RF_MPI/inc/Node.h:
---
+++ toolchain.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_COMPILER    mpicc)
+set(CMAKE_CXX_COMPILER  mpic++)
